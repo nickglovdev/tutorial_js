b89f4caac27b7b08ba9c31f4426e6c4908c74867
Nicholas <jacksaintjack@gmail.com>
2018-06-28 19:17:39 -0500

    Learned out to use the class selector

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
-/* const myHeading = document.getElementsByTagName('h1')[0];
+/* USE THIS FOR UNDERSTANDING SELECTION FOR ID
+
+const myHeading = document.getElementsByTagName('h1')[0];
 const myButton = document.getElementById('myButton');
 const myTextInput = document.getElementById('myTextInput');
 
 myButton.addEventListener('click', () => {
   myHeading.style.color = myTextInput.value;
 });
-use this for the button method
+
 */
+
+//LEARNING THE BASICS OF SELECTING TAGS
 const myList = document.getElementsByTagName('li');
 
-for (let i = 0; i <= myList.length; i++) {
+for (let i = 0; i < myList.length; i += 1) {
   myList[i].style.color = 'purple';
+}
+
+const notPurple = document.getElementsByClassName('not-purple')
+
+for (let i = 0; i < notPurple.length; i += 1) {
+  notPurple[i].style.color = 'red';
 }
