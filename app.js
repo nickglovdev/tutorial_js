cf4a598de1757c08992abfc1198405d85fb420ed
Nicholas <jacksaintjack@gmail.com>
2018-06-28 19:03:17 -0500

    Leaned how to select element by tag

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-const myHeading = document.getElementById('myHeading');
+/* const myHeading = document.getElementsByTagName('h1')[0];
 const myButton = document.getElementById('myButton');
 const myTextInput = document.getElementById('myTextInput');
 
 myButton.addEventListener('click', () => {
   myHeading.style.color = myTextInput.value;
 });
+use this for the button method
+*/
+const myList = document.getElementsByTagName('li');
+
+for (let i = 0; i <= myList.length; i++) {
+  myList[i].style.color = 'purple';
+}
